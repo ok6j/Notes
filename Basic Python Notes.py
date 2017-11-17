bc18433de6465143fa1380448bcb5b77292720e4
ChristianR <31050508+ok6j@users.noreply.github.com>
2017-11-17 15:37:20 -0800

    11.17.17
    Python notes added hello world

--- Basic Python Notes.py
+++ Basic Python Notes.py
@@ -1,4 +1,24 @@
 print("Hello World")
 print("Welcome to class!")
 
-#This
+# This is a comment
+# They are really useful!
+# The program, ignores these lines
+
+'''This is a bunch of text that will be ignored because it is in between 3 quotation marks
+'''
+# Variables
+a = "Hello World"
+print(a)
+
+b = 5
+c = 7
+print(b + c)
+print(-1 - (-5))
+print("Hello World")
+
+
+
+
+
+
